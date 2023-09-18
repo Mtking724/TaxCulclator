0a6496465ba30c4702e0aced213be7dcb956a251
motoki0724 <motokiokayasu@gmail.com>
2023-09-18 21:35:31 +0900

    change the keyboardType

--- TaxCulclator/ContentView.swift
+++ TaxCulclator/ContentView.swift
@@ -17,9 +17,11 @@ struct ContentView: View {
         VStack(spacing: 20) {
             //ここに文字を入力は入力がないときのヒント
             TextField("ここに文字を入力", text: $inputText)
+                .keyboardType(.numberPad)
             Button("計算") {
-                tax8 = Double(inputText)! * 0.08
-                tax10 = Double(inputText)! * 0.1
+                //数字以外の文字が打ち込まれた時に、代用として0を使用する。クラッシュ対策
+                tax8 = (Double(inputText) ?? 0) * 0.08
+                tax10 = (Double(inputText) ?? 0) * 0.1
             }
             .padding()
             Text("価格: \(inputText)")
