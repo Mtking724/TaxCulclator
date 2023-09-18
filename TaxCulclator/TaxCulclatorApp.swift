26e454a8008ded72097086e5f097f204edfd7d8f
motoki0724 <motokiokayasu@gmail.com>
2023-09-18 21:15:47 +0900

    Initial Commit

---
+++ TaxCulclator/TaxCulclatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxCulclatorApp.swift
+//  TaxCulclator
+//
+//  Created by Motoki Okayasu on 2023/09/18.
+//
+
+import SwiftUI
+
+@main
+struct TaxCulclatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
